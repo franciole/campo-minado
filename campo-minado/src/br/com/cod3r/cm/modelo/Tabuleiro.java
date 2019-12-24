0c12132717ba6711949924503901ea6f63efcae5
Franciole Bezerra <franciole.bezerra@gmail.com>
2019-12-24 20:48:19 -0300

    Aula 225

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -22,6 +22,16 @@ public class Tabuleiro {
 		sortearMinas();
 	}
 
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst()
+				.ifPresent(c -> c.abrir());
+	}
+
+	public void alterarMarcacao(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst()
+		.ifPresent(c -> c.alternarMarcacao());
+	}
+
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
 			for (int coluna = 0; coluna < colunas; coluna++) {
@@ -58,6 +68,19 @@ public class Tabuleiro {
 	}
 
 	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
 
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
 	}
 }
